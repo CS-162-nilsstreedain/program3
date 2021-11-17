0d6a44317a06401cf100503ff9bd6c8e0b58f7b3
Nils Streedain <25465133+nilsstreedain@users.noreply.github.com>
2021-11-17 14:55:37 -0800

    Reset Repository & Recommitted All Files

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Program 3
+//
+//  Created by Nils Streedain on 11/17/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
